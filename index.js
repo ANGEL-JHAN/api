d4823b70c3cb16acd4b04062fc8e4377194acaf0
ANGEL-JHAN <62525883angel@gmail.com>
2026-03-30 08:33:42 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-const { generarRespuesta, guardarMemoria } = require("./ai");
+const { generarRespuesta, guardarMemoria } = require("./ia");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
