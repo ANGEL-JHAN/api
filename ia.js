9698e3e2d1cbcc52106bfeaa8560f0ef8df2b632
ANGEL-JHAN <62525883angel@gmail.com>
2026-05-15 20:42:56 -0500

    Actualizar ia.js

--- ia.js
+++ ia.js
@@ -18,7 +18,7 @@ async function generarRespuesta(
     const completion =
       await client.chat.completions.create({
 
-      model: "llama3-70b-8192",
+      model: "llama-3.3-70b-versatile",
 
       messages: [
 
