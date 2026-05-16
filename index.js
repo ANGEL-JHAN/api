67afc33fc3575a86b9177a8fb5e79ee5f0498886
ANGEL-JHAN <62525883angel@gmail.com>
2026-05-16 08:55:03 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ const { v4: uuidv4 } = require("uuid");
 // =========================
 // 🔥 SUPABASE
 // =========================
-const { createClient } = require("@supabase/supabase-js");
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
+
+console.log("🔥 URL:", process.env.SUPABASE_URL);
+console.log("🔥 KEY:", process.env.SUPABASE_ANON_KEY);
 
 // =========================
 // 🔥 FIX fetch para Node / Render
